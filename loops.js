47af47c729e13550268448d8c23ea7cb56c14dc3
MrChock <jchock@punahou.edu>
2016-06-24 14:29:06 -1000

    Finished second challenge

--- loops.js
+++ loops.js
@@ -1,1 +1,11 @@
-// Do your work here
+var presidents = ["Washington", "Adams", "Jefferson", "Madison,", "Monroe"];
+
+for(var i = 0; i<presidents.length; i++){
+  console.log("The value of i is " + i);
+  console.log("The value at Index is " + presidents[i]);
+}
+var stringOfNumbers = ' ';
+for(var i = 10; i<21; i++){
+  stringOfNumbers = stringOfNumbers+i;
+}
+console.log(stringOfNumbers);
