1c21564574a14c8ed769b32759ef2298dd16b213
MrChock <jchock@punahou.edu>
2016-06-24 14:51:48 -1000

    Challenge 4

--- loops.js
+++ loops.js
@@ -1,18 +1,42 @@
 var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
 
-for(var i = 0; i<presidents.length; i++){
+function (presidents){
+	for(var i = 0; i<presidents.length; i++){
   console.log("The value of i is " + i);
   console.log("The value at Index is " + presidents[i]);
 }
+}
+
+
+
+
+
 var stringOfNumbers = ' ';
+
+function (stringOfNumbers){
 for(var i = 10; i<21; i++){
   stringOfNumbers = stringOfNumbers+i;
 }
 console.log(stringOfNumbers);
+}
+
 
 var evenNumberArray =[];
 
+function(evenNumberArray){
 for(var i = 0; i<50; i++){
   evenNumberArray.push(i*2);
 }
 console.log(evenNumberArray);
+}
+
+var oopsArray = ['turn', ,'down', ,'for', ,'what'];
+
+function(oopsArray){
+for(var i = 1; i<6; i+=2){
+  oopsArray.splice(i,1,"nope");
+}
+console.log(oopsArray);
+}
+
+
