695b4aedae8bd69eef280184a2c0b53461bcbeda
MrChock <jchock@punahou.edu>
2016-06-27 14:42:40 -1000

    Fixed up to Challenge 6

--- loops.js
+++ loops.js
@@ -1,49 +1,76 @@
-var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+//First Challenge: "Presidents"
 
-function (presidents){
+function makePresidents(){
+	var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+
 	for(var i = 0; i<presidents.length; i++){
-  console.log("The value of i is " + i);
-  console.log("The value at Index is " + presidents[i]);
+  		console.log("The value of i is " + i);
+  		console.log("The value at Index is " + presidents[i]);
+	}
 }
+makePresidents();
+
+//Second Challenge: "Make String of Numbers"
+
+function makeStringOfNumbers (){
+	var stringOfNumbers = '';
+	for(var i = 10; i<21; i++){
+	  stringOfNumbers = stringOfNumbers+i;
+	}
+    console.log(stringOfNumbers);
 }
+makeStringOfNumbers();
 
+//Third Challenge: "Add Only Evens"
 
-
-
-
-var stringOfNumbers = ' ';
-
-function (stringOfNumbers){
-for(var i = 10; i<21; i++){
-  stringOfNumbers = stringOfNumbers+i;
+function makeEvenNumberArray(){
+	var evenNumberArray =[];
+	for(var i = 0; i<50; i++){
+	  evenNumberArray.push(i*2);
+	}
+	console.log(evenNumberArray);
 }
-console.log(stringOfNumbers);
-}
+makeEvenNumberArray();
 
 
-var evenNumberArray =[];
+//Fourth Challenge: "Not Even!"
 
-function(evenNumberArray){
-for(var i = 0; i<50; i++){
-  evenNumberArray.push(i*2);
+function makeOopsArray(){
+	var oopsArray = ['turn', ,'down', ,'for', ,'what'];
+	for(var i = 1; i<6; i+=2){
+	  	oopsArray.splice(i,1,"nope");
+	}
+	console.log(oopsArray);
+	return oopsArray;
 }
-console.log(evenNumberArray);
-}
+var oopsResult = makeOopsArray();
 
-var oopsArray = ['turn', ,'down', ,'for', ,'what'];
 
-function(oopsArray1){
-for(var i = 1; i<6; i+=2){
-  oopsArray.splice(i,1,"nope");
-}
-console.log(oopsArray1);
-}
+//Fifth Challenge: "Going Backwards"
 
-function(oopsArray2){
-for(var i = 6; i>-1; i--){
-  console.log(oopsArray[i]);
+function goBackwards(oopsResult){
+	for(var i = 6; i>-1; i--){
+  		console.log(oopsResult[i]);
+	}
 }
-}
+goBackwards(oopsResult);
 
-var isNapTime = false;
-var napSchedule = [false, false, true, false, true, true];
+//Sixth Challenge: "Naptime"
+
+function makeNapSchedule(){
+	var isNapTime = false;
+	var napSchedule = [false, false, true, false, true, true];
+	function nap(schedule){
+		if(schedule[i]===true){
+			console.log("ZzZzZzZz");
+		}
+		if(schedule[i]===false){
+			console.log("Gotta get to work!");
+		}
+	}
+  	for(var i = 0; i <6; i++){
+      nap(napSchedule);
+    }
+}
+makeNapSchedule();
+  
